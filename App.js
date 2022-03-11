a08a9b1a59ff5881e1ea310eea9599925e820118
emcdanie <66241957+emcdanie@users.noreply.github.com>
2022-03-11 14:26:18 +0100

    images
    added local and uri image

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 
 
 export default function App() {
 
 return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello React Native World!</Text>
+      <Text> Hello React Native World </Text>
+      <Image source={require("./assets/favicon.png")}/>
+      <Image source={{
+        width:200,
+        height:300,
+        uri:"https://picsum.photos/200/300"}}/>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -15,7 +20,7 @@ return (
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'pink',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
