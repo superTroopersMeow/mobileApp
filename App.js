f79e2dfd7000c8c0e59f977b35fa6b73e89f10e4
emcdanie <66241957+emcdanie@users.noreply.github.com>
2022-03-11 14:36:02 +0100

    Touchable Component
    Touchable Opacity added to image

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Text,TouchableOpacity, Image, SafeAreaView } from 'react-native';
 
 
 export default function App() {
@@ -7,11 +7,13 @@ export default function App() {
 return (
     <SafeAreaView style={styles.container}>
       <Text> Hello React Native World </Text>
-      <Image source={require("./assets/favicon.png")}/>
+      <TouchableOpacity onPress={() => console.log("Image Tapped")}>
       <Image source={{
         width:200,
         height:300,
         uri:"https://picsum.photos/200/300"}}/>
+
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
